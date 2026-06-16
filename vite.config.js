cd0c35ad803dd738eed6e46d2a05f3e86a1fde16
Guilherme Silva <git@github.com:guilhermegalvaosilva/news-explorer-frontend.git>
2026-06-16 15:54:11 -0300

    Usa caminhos relativos no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Painel/',
+  base: './',
   plugins: [react()],
   build: {
     sourcemap: false,
